3f3d8a24b6a8e714e26b0b7cb0497842476e6277
paul <paulentz@gmail.com>
2023-05-05 22:08:50 -0500

    done with assignment 2, tests are passing, fixed a warning by changing .equals() method from using "that" keyword to using the class object name. Changed from:
      @Override
        public boolean equals(Object o) {  // Two objects are equal if they have the same id.
            if (this == o) return true;
            if (!(o instanceof Employer)) return false;
            Employer that = (Employer) o;
            return getId() == that.getId();
    
    to
    
      @Override
        public boolean equals(Object o) {  // Two objects are equal if they have the same id.
            if (this == o) return true;
            if (!(o instanceof Employer)) return false;
            Employer employer = (Employer) o;
            return getId() == employer.getId();

--- src/main/java/org/launchcode/techjobs/oo/JobField.java
+++ src/main/java/org/launchcode/techjobs/oo/JobField.java
@@ -1,26 +1,98 @@
 package org.launchcode.techjobs.oo;
 
-public class JobField {
+import java.util.Objects;
+
+public abstract class JobField {
 
     //Consider the following questions to help you decide what code to put in the JobField class:
-    //
+
     //What fields do ALL FOUR of the classes have in common?
-    //
+    //    private int id;
+    //    private static int nextId = 1;
+    //    private String value;
+
     //Which constructors are the same in ALL FOUR classes?
+    //  public CoreCompetency() {
+    //        this.id = nextId;
+    //        nextId++;
+    //    }
     //
+    //    public CoreCompetency(String value) {
+    //        this();
+    //        this.value = value;
+
     //What getters and setters do ALL of the classes share?
+    //    public int getId() {
+    //        return id;
+    //    }
     //
+    //    public String getValue() {
+    //        return value;
+    //    }
+    //
+    //    public void setValue(String value) {
+    //        this.value = value;
+    //    }
+
+
     //Which custom methods are identical in ALL of the classes?
-    //
+    //  @Override
+    //    public String toString() {
+    //        return value;
+    //    }
+
+    //  @Override
+    //    public int hashCode() {
+    //        return Objects.hash(id);
+    //    }
+
+
     //In JobField, declare each of the common fields.
-    //
+    private int id;
+    private static int nextId = 1;
+    private String value;
+
     //Code the constructors.
-    //
+
+
+    public JobField() {
+        id = nextId;
+        nextId++;
+    }
+
+    public JobField(String value) {
+        this();
+        this.value = value;
+    }
+
+
     //Use Generate to create the appropriate getters and setters.
-    //
+
+    public int getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+
     //Add in the custom methods.
-    //
-    //Finally, to prevent the creation of a JobField object, make this class abstract.
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 
 
 }
